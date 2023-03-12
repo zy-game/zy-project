19ac57cc21cefde037a6fb4600f0aa6f6a978f31
or <1584948787@qq.com>
2023-03-13 00:56:06 +0800

    Initial commit

---
+++ WebService/WebService/ChatGPTClient.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text;
+
+public class ChatGPTClient
+{
+    private const string endpoint = "https://api.openai.com/v1/engines/text-moderation-playground/completions";
+
+    private readonly string apiKey;
+
+    public ChatGPTClient(string apiKey)
+    {
+        this.apiKey = apiKey;
+    }
+
+    public async Task<string> GenerateTextAsync(string prompt)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
+            string reqult = prompt;
+            while (true)
+            {
+                var requestBody = new
+                {
+                    prompt = reqult,
+                    temperature = 0.7,
+                    max_tokens = 50,
+                    top_p = 1,
+                    frequency_penalty = 0,
+                    presence_penalty = 0,
+                    stop = "##END##"
+                };
+                var requestContent = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(endpoint, requestContent, CancellationToken.None);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return "error:" + response.StatusCode.ToString();
+                }
+                string responseContent = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(responseContent);
+                reqult += json["choices"][0]["text"];
+                //                  reqult += responseContent;
+                //                  {
+                //                      "id": "cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7",
+                //"object": "text_completion",
+                //"created": 1589478378,
+                //"model": "text-davinci-003",
+                //"choices": [
+                //  {
+                //                          "text": "\n\nThis is indeed a test",
+                //    "index": 0,
+                //    "logprobs": null,
+                //    "finish_reason": "length"
+                //  }
+                //],
+                //"usage": {
+                //                          "prompt_tokens": 5,
+                //  "completion_tokens": 7,
+                //  "total_tokens": 12
+                //}
+                //                  }
+                // Check for termination condition here, e.g. a specific response from the API
+                string state = json["choices"][0]["finish_reason"].ToString();
+                if (state == "stop")
+                {
+                    break;
+                }
+                Thread.Sleep(100);
+            }
+            return reqult;
+        }
+    }
+}
---
+++ WebService/WebService/Controllers/Web.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class web : ControllerBase
+    {
+        // GET: api/<Web>
+        [HttpGet]
+        public string Get()
+        {
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Response.Headers.Add("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+            var result = "";
+            result += "<li class=\"layui-timeline-item\">";
+            result += "<i class=\"layui-icon layui-timeline-axis\">&#xe63f; </i>";
+            result += "<div class=\"layui-timeline-content layui-text\">";
+            result += "<h3 class=\"layui-timeline-title\">";
+            result += "<a href=\"test.html\">";
+            result += "8月18日</a></h3><p> layui 2.0 的一切准备工作似乎都已到位。发布之弦，一触即发。<br>不枉近百个日日夜夜与之为伴。因小而大，因弱而强。<br>无论它能走多远，抑或如何支撑？至少我曾倾注全心，无怨无悔...</p></div></li>";
+            return result;
+        }
+
+        // GET api/<Web>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Web>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Web>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Web>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
